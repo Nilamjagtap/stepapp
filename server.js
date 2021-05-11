ef5a66ae38a1e669fc64fc30c9ba261c41376537
Nilam <nilamnjagtap@gmail.com>
2021-05-11 11:46:52 +0530

    AngularAuth routing added

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/stepapp'));
+app.use(express.static('./dist/AngularAuth'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/stepapp/'}),
+    res.sendFile('index.html', {root: 'dist/AngularAuth/'}),
 );
 
 // Start the app by listening on the default Heroku port
